b14ba12c38d52a5c6c5289fe5019a578e8a9129e
Marcus Vinicius Freitas Margarites <marcus.vinicius@globant.com>
2025-10-30 11:16:01 -0300

    Adding the now() function.

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import datetime
+from zoneinfo import ZoneInfo
+
 from fastmcp import FastMCP
 
 mcp = FastMCP("mvfm's mcp server")
@@ -6,6 +9,12 @@ mcp = FastMCP("mvfm's mcp server")
 def add(a: int, b: int) -> int:
     """Adds two numbers"""
     return a + b
+
+
+@mcp.tool
+def now() -> datetime.datetime:
+    """Returns the current date and time"""
+    return datetime.datetime.now(ZoneInfo("America/Sao_Paulo"))
 
 
 if __name__ == "__main__":
